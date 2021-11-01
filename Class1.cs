9c4100661edc2f3f608a762a23117fb496e8c347
Алияр Молдабеков <aliyar.m03@gmail.com>
2021-11-02 02:11:09 +0700

    I doing lb3

---
+++ Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace LB2_IT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] z = new int[13];
+
+            Random rand = new Random();
+            for (int i = 0; i < z.Length; i++)
+                z[i] = rand.Next(-100, 100);
+            for (int i = 0; i < z.Length; i++)
+            {
+                Console.Write(z[i] + " ");
+            }
+
+            bool sign = z[0] > 0 ? (true) : (false);
+            //true полож false отр
+            for (int i = 0; i < z.Length - 1; i++)
+            {
+                if (sign == true)
+                    for (int j = i + 1; sign == true; j++)
+                    {
+                        if (z[j] > 0)
+                        {
+                            z[j] = 0;
+                        }
+                        else if (z[j] < 0)
+                        {
+                            sign = !sign;
+                        }
+
+                    }
+                else if (sign == false)
+                    for (int j = i + 1; sign == false; j++)
+                    {
+                        if (z[j] < 0)
+                        {
+                            z[j] = 0;
+                        }
+                        else if (z[j] > 0)
+                        {
+                            sign = !sign;
+                        }
+
+                    }
+                sign = !sign;
+            }
+
+            Console.WriteLine("Вывод отсортированного массива");
+            for (int i = 0; i < z.Length; i++)
+            {
+                Console.Write(z[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace LB3_IT
 {
@@ -8,51 +6,70 @@ namespace LB3_IT
     {
         static void Main(string[] args)
         {
-            //int n = Convert.ToInt32(Console.ReadLine());           
-            //int[] array = new int[n];
-
-            int[] array = new int[10] { -1, -2, -3, 4, 5, 6, -7, 8, 9, -10 };
-            //Random rand = new();
-            //for (int i = 0; i < array.Length; i++)
-            //{
-            //    array[i] = rand.Next(-100, 100);
-            //    Console.Write(array[i] + " ");
-            //}
-
-            bool sign = array[0] > 0;
-            //если сигн тру, ищем отрицательное, если сигн фолс то ищем положительное
-            for (int i = 0; i < array.Length - 1; i++)
+            Console.WriteLine("Введите длину масссива:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            Random rand = new();
+            for (int i = 0; i < array.Length; i++)
             {
-                int j = i + 1;
-                if (array[i] == 0)
-                {
-                    //sign = !sign;
-                }
-                else if (sign == true)
-                {
-                    if (array[j] > 0)
+                array[i] = rand.Next(-100, 100); // присваиваем в массив с индексом i рандомное число в диапазоне от -100 до 10
+                Console.Write(array[i] + " ");
+            }
+
+            bool sign = array[0] > 0; //задаём переменную sign, которая проверяет знак первого(нулевого) элемента массива
+            
+            for (int i = 0; i < array.Length - 1; i++) // который перебирает массив array с шагом в один, начиная с нулевого элемента, до элемента равного количеству элементов
+            {
+                if (sign == true && array[i] != 0) //так как sign - булевое значение, в случае если он равен true и проверяемый элемент не равен нулю, то выполняется цикл for
+                    for (int j = i + 1; sign == true && j < array.Length; j++) // который 
                     {
-                        array[j] = 0;
+                        if (array[j] > 0)
+                        {
+                            array[j] = 0;
+                        }
+                        else if (array[j] < 0)
+                        {
+                            sign = !sign;
+                        }
                     }
-                }
-                else if (sign == false)
-                {
-                    if (array[j] < 0)
+                else if (sign == false && array[i] != 0)
+                    for (int j = i + 1; sign == false && j < array.Length; j++)
                     {
-                        array[j] = 0;
+                        if (array[j] < 0)
+                        {
+                            array[j] = 0;
+                        }
+                        else if (array[j] > 0)
+                        {
+                            sign = !sign;
+                        }
                     }
+            }
+
+            int newLength = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != 0)
+                    newLength++;
+            }
+            int[] newArray = new int[newLength];
+            int newIndex = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != 0)
+                {
+                    newArray[newIndex] = array[i];
+                    newIndex++;
                 }
-                sign = !sign;
             }
 
             Console.WriteLine();
-            Console.WriteLine("Вывод отсортированного массива");
-            for (int i = 0; i < array.Length; i++)
+            Console.WriteLine("Вывод преобразованного массива:");
+            for (int i = 0; i < newArray.Length; i++)
             {
-                Console.Write(array[i] + " ");
+                Console.Write(newArray[i] + " ");
             }
             Console.ReadLine();
-            //Console.WriteLine(newArray);
         }
     }
 }
